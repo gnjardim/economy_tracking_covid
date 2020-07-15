83d26ae5e358006d01f23388865037b8c2d8e02f
gnjardim <48603966+gnjardim@users.noreply.github.com>
2020-07-14 21:33:02 -0300

    add comparison

--- code/comparing_data.R
+++ code/comparing_data.R
@@ -3,13 +3,13 @@
 
 
 # packages ----------------------------------------------------------------
-
 library(tidyverse)
 library(lubridate)
 library(dplyr)
 library(zoo)
 library(readr)
 library(readxl)
+library(writexl)
 
 # source ------------------------------------------------------------------
 bases_estados_df <- read_csv("meta/bases_estados_df.csv")
@@ -18,24 +18,32 @@ PET_Brazil_20200705 <- read_excel("meta/PET-Brazil_20200705.xlsx")
 
 # rio de janeiro ----------------------------------------------------------
 bases_estados_rj <- bases_estados_df %>% 
-    dplyr:: filter(state == 'RJ')%>%
-    dplyr:: select(retail_and_recreation_percent_change_from_baseline,
+    dplyr::select(state, date,
+                   retail_and_recreation_percent_change_from_baseline,
                    grocery_and_pharmacy_percent_change_from_baseline,
                    workplaces_percent_change_from_baseline,
                    mobility,
                    activity,
-                   smth_mobility,
-                   date)%>%
-    dplyr:: arrange(date)
+                   moving_avg_mobility = smth_mobility,
+                   date) %>%
+    dplyr::arrange(state, date)
     
 
 PET_Brazil_rj <- PET_Brazil_20200705 %>% 
-    dplyr:: filter (ISO == 'RJ')%>%
-    dplyr:: arrange(Date)
+    dplyr::arrange(Date)
 
 
-comparacao <- full_join(PET_Brazil_rj,bases_estados_rj, by = c('Date'= 'date'))%>% 
-    dplyr::select(Date,Mobility, mobility, retail_and_recreation_percent_change_from_baseline,
+comparacao <- full_join(PET_Brazil_rj,bases_estados_rj, 
+                        by = c('Date'= 'date', "ISO" = "state")) %>% 
+    dplyr::select(ISO, Date, PET_Mobility = Mobility, mobility, moving_avg_mobility, 
+                  retail_and_recreation_percent_change_from_baseline,
                   grocery_and_pharmacy_percent_change_from_baseline,
-                  workplaces_percent_change_from_baseline)
+                  workplaces_percent_change_from_baseline) %>% 
+    dplyr::arrange(ISO, Date) %>% 
+    filter(!is.na(PET_Mobility))
+
+
+# export ------------------------------------------------------------------
+write_xlsx(comparacao, "Comparacao.xlsx")
+    
 
