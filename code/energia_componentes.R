db39b7023f54777c2de198e1b24d942680f1dd7c
gnjardim <48603966+gnjardim@users.noreply.github.com>
2020-08-05 17:52:22 -0300

    organize report

--- code/energia_componentes.R
+++ code/energia_componentes.R
@@ -1,8 +1,11 @@
 # packages ----------------------------------------------------------------
 library(tidyverse)
+library(lubridate)
+library(janitor)
+library(zoo)
 
 
-# file --------------------------------------------------------------------
+# load data ---------------------------------------------------------------
 energy_ramo <- read_csv2("input/energia_data.csv") %>% 
     rename(ramo = `Ramo de atividade`) %>% 
     mutate(Estado = iconv(Estado, from = "UTF-8", to = "ASCII//TRANSLIT") %>% 
@@ -20,10 +23,38 @@ energy_ramo <- read_csv2("input/energia_data.csv") %>%
            ramo == "SERVICOS"               ~ "SERVICOS",
            ramo == "COMERCIO"               ~ "SERVICOS",
            ramo == "TRANSPORTE"             ~ "SERVICOS",
-           ramo == "TELECOMUNICACOES"       ~ "SERVICOS")
-           )
+           ramo == "TELECOMUNICACOES"       ~ "SERVICOS"),
+           semana = week(data),
+           ano    = year(data)) %>% 
+    arrange(estado, data, ramo) %>% 
+    group_by(estado, ramo, semana, ano) %>% 
+    summarise(consumo = sum(consumo_m_wh),
+              data    = min(data))
 
 energy_ramo %>% filter(is.na(Componente)) %>% select(ramo) %>% unique() %>% View()
 
                        
-                       
+# plots -------------------------------------------------------------------
+# somente ACR
+acr <- energy_ramo %>% filter(ramo == "ACR") %>% 
+    ggplot(aes(x = data, y = consumo)) +
+    geom_line(aes(color = ramo)) +
+    facet_wrap(~estado, ncol = 3, scales = "free_y")
+
+# sem ACR
+no_acr <- energy_ramo %>% filter(ramo != "ACR") %>% 
+    ggplot(aes(x = data, y = consumo)) +
+    geom_line(aes(color = ramo)) +
+    facet_wrap(~estado, ncol = 3, scales = "free_y")
+
+
+# export ------------------------------------------------------------------
+h <- 24
+w <- 18
+
+ggsave("onlyACR.pdf", acr,
+       height = h, width = w)
+
+ggsave("noACR.pdf", no_acr,
+       height = h, width = w)
+
