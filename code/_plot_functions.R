9c1c67e5954478ae061e5d3d172efb6df462c615
gnjardim <48603966+gnjardim@users.noreply.github.com>
2020-08-06 15:04:49 -0300

    minor changes

--- code/_plot_functions.R
+++ code/_plot_functions.R
@@ -145,22 +145,22 @@ plot_comparacao_estado <- function(UF){
 
 
 plot_ramo <- function(reg){
-  df <- total_energy_ramo%>%
-    filter(data <= as.Date("2020-02-25") & regiao == reg)%>%
-    group_by(ramo,estado)%>%
+  
+  df <- total_energy_ramo %>%
+    filter(data <= as.Date("2020-02-25") & regiao == reg) %>%
+    group_by(ramo, estado) %>%
     summarize(Media_Consumo_MhW = mean(dif_consumo, na.rm = TRUE),
-              Media_Dif_Perc    =mean(dif_baseline, na.rm = TRUE)*100)
+              Media_Dif_Perc    = mean(dif_baseline, na.rm = TRUE)*100)
            
   max_lim <- as.integer(ceiling(max(df$Media_Dif_Perc)))
-  
   min_lim <- as.integer(floor(min(df$Media_Dif_Perc)))
   
   p <- df %>% 
     ggplot(aes(ramo)) +
-    geom_col(mapping = aes(y = Media_Dif_Perc),position = "dodge")+ 
+    geom_col(mapping = aes(y = Media_Dif_Perc), position = "dodge")+ 
     scale_x_discrete(name = "Ramo")+
-    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))+
-    scale_y_continuous(name = "Média Percentual", limits = c(min_lim,max_lim))+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))+
+    scale_y_continuous(name = "Média Percentual", limits = c(min_lim, max_lim))+
     facet_wrap(~estado, scales = "free") 
   
   return(p)
