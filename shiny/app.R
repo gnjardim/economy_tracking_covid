af5cc86df59dc48f4203b90e2336e5c0169a7d2e
mmittelbach <mariamittelbach@gmail.com.br>
2020-08-14 10:33:36 -0300

    apply shinydashboard

--- shiny/app.R
+++ shiny/app.R
@@ -1,66 +1,78 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
 #
 
-
-# packages ----------------------------------------------------------------
 library(shiny)
-library(tidyverse)
-library(grid)
-library(cowplot)
-library(gridExtra)
+library(shinydashboard)
+library(shinyWidgets)
 library(plotly)
+library(shinycssloaders)
+library(widgetframe)
+library(ggiraph)
 
-
-# source ------------------------------------------------------------------
-source('load.R', encoding = 'UTF-8')
-
-
-# app ---------------------------------------------------------------------
-# Define UI for application
-ui <- fluidPage(
+# Define UI for application that draws a histogram
+ui <- shinyUI(fluidPage(
     
     # Application title
-    titlePanel("Atividade x COVID"),
-    
-    # Sidebar with inputs
+    titlePanel(""),
+    useShinydashboard(),
+    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             selectInput(inputId  = "UF",
                         label    = "Região",
                         choices  = choices,
-                        selected = "Brasil"),
+                        selected = "Brasil")
             
-            radioButtons(inputId = "tipo", 
-                         label   = "",
-                         choices = c("Índice de Atividade" = "atividade",
-                                     "Observado x Predito" = "pred"))
-        ),
+            ),
         
-        # Main panel for displaying outputs ----
+        # Show a plot of the generated distribution
         mainPanel(
-            
-            # Output: plotly ----
-            plotlyOutput("plot")
+            tabsetPanel(type = "tabs",
+                        
+                        tabPanel("Índice de Atividade",
+                                 box(title = 'Informações - clique no "-" para colapsar', collapsible = TRUE, solidHeader = TRUE,
+                                     status="warning",
+                                     p("INFORMACOES")),
+                                 withSpinner(plotlyOutput("plot_atividade")))),                        
+                        tabPanel("Observado x Predito",
+                                box(title = 'Informações - clique no "-" para colapsar', collapsible = TRUE, solidHeader = TRUE,
+                                    status="warning",
+                                        p("INFORMACOES")),
+                                    withSpinner(plotlyOutput("plot_pred")))
             
         )
     )
 )
+)
 
-# Define server logic required
+
 server <- function(input, output) {
+    # The currently selected tab from the first box
+    output$plot_atividade <- renderPlot({
+        
+        plot_shiny(UF = input$UF, 
+                   tipo = 'atividade')
+        
+    })
     
-    output$plot <- renderPlotly({
-        plot_shiny(UF   = input$UF, 
-                   tipo = input$tipo)
+    output$plot_pred <- renderPlot({
+        
+        plot_shiny(UF = input$UF, 
+                   tipo = 'pred')
+        
     })
 }
 
 
+
+
 # Run the application 
 shinyApp(ui, server)
+
+
